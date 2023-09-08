2a4607978f4d696d9037b8e23960232cabf016ee
Mizakson <maxwellizakson@gmail.com>
2023-09-08 13:15:29 -0400

    Edit formatting, modify operate function

--- js/javascript.js
+++ js/javascript.js
@@ -1,36 +1,30 @@
-/* 
-4 basic math operator functions
-
-add
-subtract
-multiply
-divide
-*/
+// ADD DOM ELEMENTS HERE...
 
 function add(num1, num2) {
     return num1 + num2;
 };
 
-
-
 function subtract(num1, num2) {
     return num1 - num2;
 };
-
-
 
 function multiply(num1, num2) {
     return num1 * num2;
 };
 
-
-
 function divide(num1, num2) {
     return num1 / num2;
 };
 
-
-
-// function operate(num1, num2, operator) {
-//     return operator(num1, num2);
-// };
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
+};
