3eb29fdf4b30ac114f8cf28ed1f51c6bda3026c9
Mizakson <maxwellizakson@gmail.com>
2023-09-18 10:19:50 -0400

    Complete step 5 of project

--- js/javascript.js
+++ js/javascript.js
@@ -30,31 +30,20 @@ function operate(num1, num2, operator) {
 };
 
 // // DOM ELEMENTS
-// const calc = document.querySelector('.calc-container');
-// const display = document.querySelector('.display-box');
-// const operand = document.querySelector('.operand-container');
-// const digit = document.querySelector('.digits-container');
+const calc = document.querySelector('.calc-container');
+const display = document.querySelector('.display-box');
+const button = document.querySelector('.button');
 
-// // const one = document.querySelector('.digit[value="1"]');
-// // const two = document.querySelector('.digit[value="2"]');
-// // const three = document.querySelector('.digit[value="3"]');
-// // const four = document.querySelector('.digit[value="4"]');
-// // const five = document.querySelector('.digit[value="5"]');
-// // const six = document.querySelector('.digit[value="6"]');
-// // const seven = document.querySelector('.digit[value="7"]');
-// // const eight = document.querySelector('.digit[value="8"]');
-// // const nine = document.querySelector('.digit[value="9"]');
+// populate display on button click
+let digits = document.querySelectorAll('.digit');
+for (let i = 0; i < digits.length; i++) {
+    let digit = digits[i];
+    digit.addEventListener('click', () => {
+        document.getElementById('result').innerHTML += digit.innerHTML
+    })
+};
 
-// const clear = document.querySelector('button[value="C"]');
-// const equals = document.querySelector('button[value="="]');
-// const displayText = document.querySelector('.result');
 
-// let initialValue = '';
-// const updateValue = (value) => {
-//     initialValue = value;
-// }
 
-// digit.onclick = () => {
-//     displayText.textContent = updateValue('Pressed');
-// }
+
 
