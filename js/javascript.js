bea24f541dccb2e182d695a52ea0fb8c9fd28ef2
Mizakson <maxwellizakson@gmail.com>
2023-09-18 12:49:09 -0400

    Add dvide by 0 error msg

--- js/javascript.js
+++ js/javascript.js
@@ -27,45 +27,67 @@ function operate(num1, num2, operator) {
         case "/":
             return divide(num1, num2);
     }
+    if ((num2 = 0) && (operator = '/')) {
+        return "CAN'T DIVIDE BY 0!";
+    }
 };
 
 // // DOM ELEMENTS
 const calc = document.querySelector('.calc-container');
 const display = document.querySelector('.display-box');
 const button = document.querySelector('.button');
+const clear = document.getElementById('clear');
+const equalSign = document.getElementById('equals');
 
 // populate display on button click
 let digits = document.querySelectorAll('.digit');
 let operands = document.querySelectorAll('.operand');
 
+let num1 = "";
+let num2 = "";
+let operator = "";
 
-function displayNum () {
+
+digits.forEach(digit => {
+    digit.addEventListener('click', e => {
+        if (operator === "") {
+            num1 += e.target.innerHTML;
+            console.log(num1);
+        } else {
+            num2 += e.target.innerHTML;
+            console.log(num2);
+        }
+    });
+});
+
+operands.forEach(op => {
+    op.addEventListener('click', e => {
+        operator = e.target.innerHTML;
+        console.log(operator);
+    }) 
+});
+
+function showNum () {
     for (let i = 0; i < digits.length; i++) {
         let digit = digits[i];
         digit.addEventListener('click', () => {
-            const clicks = document.getElementById('result').innerHTML += digit.innerHTML;
-            console.log(clicks);
-            return clicks;
+            document.getElementById('result').innerHTML += digit.innerHTML;
         })
-    };    
-};
-
-function displayOperand () {
+    }; 
+}
+   
+function showOperator () {
     for (let i = 0; i < operands.length; i++) {
         let operand = operands[i];
         operand.addEventListener('click', () => {
-            const clicks = document.getElementById('result').innerHTML += operand.innerHTML;
-            console.log(clicks);
-            return clicks;
+            document.getElementById('result').innerHTML += operand.innerHTML;
         })
     }
+    equalSign.addEventListener('click', () => {
+        document.getElementById('result').innerHTML += equalSign.innerHTML;
+    })
 }
 
 
-displayNum();
-displayOperand();
-
-
-
-
-
+showNum();
+showOperator();    
