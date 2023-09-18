7e904853de3cd2b0b86b276a27de163be6dd1488
Mizakson <maxwellizakson@gmail.com>
2023-09-18 13:32:15 -0400

    Finish project
    Very primitive solution

--- js/javascript.js
+++ js/javascript.js
@@ -16,22 +16,6 @@ function divide(num1, num2) {
     return num1 / num2;
 };
 
-function operate(num1, num2, operator) {
-    switch (operator) {
-        case "+":
-            return add(num1, num2);
-        case "-":
-            return subtract(num1, num2);
-        case "*":
-            return multiply(num1, num2);
-        case "/":
-            return divide(num1, num2);
-    }
-    if ((num2 = 0) && (operator = '/')) {
-        return "CAN'T DIVIDE BY 0!";
-    }
-};
-
 // // DOM ELEMENTS
 const calc = document.querySelector('.calc-container');
 const display = document.querySelector('.display-box');
@@ -46,6 +30,7 @@ let operands = document.querySelectorAll('.operand');
 let num1 = "";
 let num2 = "";
 let operator = "";
+let result = "";
 
 
 
@@ -86,8 +71,40 @@ function showOperator () {
     }
     equalSign.addEventListener('click', () => {
         document.getElementById('display-text').innerHTML += equalSign.innerHTML;
+        console.log(equalSign.innerHTML);
+        num1 = parseInt(num1);
+        num2 = parseInt(num2);
+        result = operate(num1, num2, operator);
+        document.getElementById('display-text').innerHTML += result;
+        console.log(result);
+        if (num2 == 0 && operator == '/') {
+            result = 'ERROR';
+            document.getElementById('display-text').innerHTML = result;
+            alert("You can't divide by 0!");
+            console.log('DIVIDE BY 0 ERROR');
+        }
+    })
+    clear.addEventListener('click', function () {
+        document.getElementById('display-text').innerHTML = "";
+        num1 = "";
+        num2 = "";
+        operator = "";
+        console.log('CLEARED');
     })
 }
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "x":
+            return multiply(num1, num2);
+        case "/":    
+        return divide(num1, num2);
+    }
+};
 
 
 showNum();
